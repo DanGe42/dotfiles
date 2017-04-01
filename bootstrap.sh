14bd54b23049b8c33e6d6ef27ab123af404fb054
Daniel Ge <daniel@dge.io>
2017-04-01 00:19:23 -0700

    Update setup for Neovim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-set -eu
+set -o errexit
+set -o nounset
+set -o xtrace
 
 # Update submodules first
 git submodule init
@@ -8,26 +10,44 @@ git submodule update
 
 # Set up z script
 mkdir -p ~/bin
-ln -s $PWD/bin/z/z.sh ~/bin
-ln -s $PWD/bin/z/z.1 ~/bin
+ln -s "$PWD"/bin/z/z.sh ~/bin
+ln -s "$PWD"/bin/z/z.1 ~/bin
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s $PWD/dotfiles/tmux.conf ~/.tmux.conf
+ln -s "$PWD"/dotfiles/tmux.conf ~/.tmux.conf
 
-ln -s $PWD/themes ~/.themes
+ln -s "$PWD"/themes ~/.themes
 
 # Set up ZSH
 git clone git://github.com/tarjoilija/zgen ~/bin/zgen
-ln -s $PWD/dotfiles/zshrc ~/.zshrc
-ln -s $PWD/dotfiles/aliases ~/.aliases
+ln -s "$PWD"/dotfiles/zshrc ~/.zshrc
+ln -s "$PWD"/dotfiles/aliases ~/.aliases
 
 # Set up Git
-ln -s $PWD/dotfiles/gitconfig ~/.gitconfig
+ln -s "$PWD"/dotfiles/gitconfig ~/.gitconfig
 
 # Set up Vim
-ln -s $PWD/dotfiles/vimrc ~/.vimrc
+ln -s "$PWD"/dotfiles/vimrc ~/.vimrc
 mkdir -p ~/.vimtmp/backup
 mkdir -p ~/.vimtmp/swp
+mkdir -p ~/.vim
+# Set up Neovim
+mkdir -p ~/.config
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
 
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-vim +NeoBundleInstall +qall
+# Install Python support for Neovim
+if which pip2 2> /dev/null
+then
+    pip2 install --upgrade neovim || true
+fi
+if which pip3 2> /dev/null
+then
+    pip3 install --upgrade neovim || true
+fi
+
+# Set up dein.vim (vim plugin manager)
+dein_tmp=$(mktemp -t "dein_installer")
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > "$dein_tmp"
+sh "$dein_tmp" ~/.vim/bundles
+vim +"call dein#install()" +qall
