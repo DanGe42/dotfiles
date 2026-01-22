a6ca669db4da93f03d689b657dda3667701f9602
Daniel Ge <dge@squareup.com>
2026-01-22 14:14:19 -0800

    bootstrap: break up script into subcommands

--- bootstrap.sh
+++ bootstrap.sh
@@ -27,34 +27,88 @@ safe_clone() {
     fi
 }
 
-echo "=== Updating submodules ==="
+# Setup functions for each component
+setup_z() {
+    echo "=== Setting up z script ==="
+    mkdir -p ~/bin
+    safe_ln "$PWD/bin/z/z.sh" ~/bin/z.sh
+    safe_ln "$PWD/bin/z/z.1" ~/bin/z.1
+}
+
+setup_tmux() {
+    echo "=== Setting up tmux ==="
+    safe_clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    safe_ln "$PWD/dotfiles/tmux.conf" ~/.tmux.conf
+}
+
+setup_themes() {
+    echo "=== Setting up themes ==="
+    safe_ln "$PWD/themes" ~/.themes
+}
+
+setup_zsh() {
+    echo "=== Setting up ZSH ==="
+    safe_clone https://github.com/tarjoilija/zgen ~/bin/zgen
+    safe_ln "$PWD/dotfiles/zshrc" ~/.zshrc
+    safe_ln "$PWD/dotfiles/aliases" ~/.aliases
+}
+
+setup_git() {
+    echo "=== Setting up Git ==="
+    safe_ln "$PWD/dotfiles/gitconfig" ~/.gitconfig
+}
+
+setup_vim() {
+    echo "=== Setting up Vim and Neovim ==="
+    # This vimrc should work on systems with vim but without neovim
+    safe_ln "$PWD/dotfiles/vimrc" ~/.vimrc
+
+    # Neovim specific setup
+    mkdir -p ~/.config
+    safe_ln "$PWD/dotfiles/nvim" ~/.config/nvim
+}
+
+# Always update submodules first
+echo "Initializing and updating submodules first..."
 git submodule init
 git submodule update
 
-echo "=== Setting up z script ==="
-mkdir -p ~/bin
-safe_ln "$PWD/bin/z/z.sh" ~/bin/z.sh
-safe_ln "$PWD/bin/z/z.1" ~/bin/z.1
-
-echo "=== Setting up tmux ==="
-safe_clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-safe_ln "$PWD/dotfiles/tmux.conf" ~/.tmux.conf
-
-echo "=== Setting up themes ==="
-safe_ln "$PWD/themes" ~/.themes
-
-echo "=== Setting up ZSH ==="
-safe_clone https://github.com/tarjoilija/zgen ~/bin/zgen
-safe_ln "$PWD/dotfiles/zshrc" ~/.zshrc
-safe_ln "$PWD/dotfiles/aliases" ~/.aliases
-
-echo "=== Setting up Git ==="
-safe_ln "$PWD/dotfiles/gitconfig" ~/.gitconfig
-
-echo "=== Setting up Vim and Neovim ==="
-# This vimrc should work on systems with vim but without neovim
-safe_ln "$PWD/dotfiles/vimrc" ~/.vimrc
-
-# Neovim specific setup
-mkdir -p ~/.config
-safe_ln "$PWD/dotfiles/nvim" ~/.config/nvim
+# Parse subcommands
+if [ $# -eq 0 ] || [ "$1" = "all" ]; then
+    # Run all setup functions
+    setup_z
+    setup_tmux
+    setup_themes
+    setup_zsh
+    setup_git
+    setup_vim
+else
+    # Run specific setup function(s)
+    for cmd in "$@"; do
+        case "$cmd" in
+            z)
+                setup_z
+                ;;
+            tmux)
+                setup_tmux
+                ;;
+            themes)
+                setup_themes
+                ;;
+            zsh)
+                setup_zsh
+                ;;
+            git)
+                setup_git
+                ;;
+            vim)
+                setup_vim
+                ;;
+            *)
+                echo "Unknown subcommand: $cmd"
+                echo "Available subcommands: z, tmux, themes, zsh, git, vim, all"
+                exit 1
+                ;;
+        esac
+    done
+fi
