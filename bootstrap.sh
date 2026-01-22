7e845fe587b5bb10351ec86ef19e1f54381600a1
Daniel Ge <dge@squareup.com>
2026-01-21 18:57:03 -0800

    bootstrap: make it more robust

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,50 +4,67 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+# Helper function to safely create symlinks
+safe_ln() {
+    src="$1"
+    dest="$2"
+    if [ -e "$dest" ]; then
+        echo "WARNING: $dest already exists, skipping link from $src"
+    else
+        ln -s "$src" "$dest"
+    fi
+}
+
+# Helper function to safely clone git repos
+safe_clone() {
+    repo="$1"
+    dest="$2"
+    if [ -e "$dest" ]; then
+        echo "WARNING: $dest already exists, skipping clone of $repo"
+    else
+        git clone "$repo" "$dest"
+    fi
+}
+
 # Update submodules first
 git submodule init
 git submodule update
 
 # Set up z script
 mkdir -p ~/bin
-ln -s "$PWD"/bin/z/z.sh ~/bin
-ln -s "$PWD"/bin/z/z.1 ~/bin
+safe_ln "$PWD/bin/z/z.sh" ~/bin/z.sh
+safe_ln "$PWD/bin/z/z.1" ~/bin/z.1
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s "$PWD"/dotfiles/tmux.conf ~/.tmux.conf
+# Set up tmux
+safe_clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+safe_ln "$PWD/dotfiles/tmux.conf" ~/.tmux.conf
 
-ln -s "$PWD"/themes ~/.themes
+safe_ln "$PWD/themes" ~/.themes
 
 # Set up ZSH
-git clone git://github.com/tarjoilija/zgen ~/bin/zgen
-ln -s "$PWD"/dotfiles/zshrc ~/.zshrc
-ln -s "$PWD"/dotfiles/aliases ~/.aliases
+safe_clone https://github.com/tarjoilija/zgen ~/bin/zgen
+safe_ln "$PWD/dotfiles/zshrc" ~/.zshrc
+safe_ln "$PWD/dotfiles/aliases" ~/.aliases
 
 # Set up Git
-ln -s "$PWD"/dotfiles/gitconfig ~/.gitconfig
+safe_ln "$PWD/dotfiles/gitconfig" ~/.gitconfig
 
 # Set up Vim
-ln -s "$PWD"/dotfiles/vimrc ~/.vimrc
+safe_ln "$PWD/dotfiles/vimrc" ~/.vimrc
 mkdir -p ~/.vimtmp/backup
 mkdir -p ~/.vimtmp/swp
 mkdir -p ~/.vim
 # Set up Neovim
 mkdir -p ~/.config
-ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
-
-# Install Python support for Neovim
-if which pip2 2> /dev/null
-then
-    pip2 install --upgrade neovim || true
-fi
-if which pip3 2> /dev/null
-then
-    pip3 install --upgrade neovim || true
-fi
+safe_ln ~/.vim ~/.config/nvim
+safe_ln ~/.vimrc ~/.config/nvim/init.vim
 
 # Set up dein.vim (vim plugin manager)
-dein_tmp=$(mktemp -t "dein_installer")
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > "$dein_tmp"
-sh "$dein_tmp" ~/.vim/bundles
-vim +"call dein#install()" +qall
+if [ ! -e ~/.vim/bundles ]; then
+    dein_tmp=$(mktemp -t "dein_installer")
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > "$dein_tmp"
+    sh "$dein_tmp" ~/.vim/bundles
+    vim +"call dein#install()" +qall
+else
+    echo "WARNING: ~/.vim/bundles already exists, skipping dein.vim setup"
+fi
